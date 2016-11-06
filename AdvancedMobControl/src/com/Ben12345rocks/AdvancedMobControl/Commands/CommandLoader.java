78c916afaea6f78d7b31bcf58a431f7ef2b3e936
Ben12345rocks <benbergen12@gmail.com>
2016-11-06 15:52:20 -0500

    Add more to gui

--- AdvancedMobControl/src/com/Ben12345rocks/AdvancedMobControl/Commands/CommandLoader.java
+++ AdvancedMobControl/src/com/Ben12345rocks/AdvancedMobControl/Commands/CommandLoader.java
@@ -22,6 +22,7 @@ import com.Ben12345rocks.AdvancedCore.Util.Inventory.BInventory;
 import com.Ben12345rocks.AdvancedCore.Util.Inventory.BInventory.ClickEvent;
 import com.Ben12345rocks.AdvancedCore.Util.Inventory.BInventoryButton;
 import com.Ben12345rocks.AdvancedCore.Util.ValueRequest.ValueRequest;
+import com.Ben12345rocks.AdvancedCore.Util.ValueRequest.Listeners.BooleanListener;
 import com.Ben12345rocks.AdvancedCore.Util.ValueRequest.Listeners.NumberListener;
 import com.Ben12345rocks.AdvancedCore.Util.ValueRequest.Listeners.StringListener;
 import com.Ben12345rocks.AdvancedMobControl.Main;
@@ -155,13 +156,13 @@ public class CommandLoader {
 
 		plugin.advancedMobControlCommands.add(new CommandHandler(new String[] {
 				"Entity", "(Entity)" }, "AdvancedMobControl.Entity",
-				"Open GUI to edit entities") {
+				"Open GUI to edit entities", false) {
 
 			@Override
 			public void execute(CommandSender sender, String[] args) {
-				if (!(sender instanceof Player)) {
-					return;
-				}
+				Utils.getInstance().setPlayerMeta((Player) sender, "Entity",
+						args[1]);
+
 				BInventory inv = new BInventory("EntityGUI: " + args[1]);
 
 				ArrayList<String> lore = new ArrayList<String>();
@@ -599,6 +600,118 @@ public class CommandLoader {
 									}
 								});
 
+					}
+				});
+
+				inv.addButton(inv.getNextSlot(), new BInventoryButton(
+						"DisableRightClick", new String[] { "Currently: "
+								+ ConfigEntity.getInstance()
+										.getDisableNormalClick(args[1]) },
+						new ItemStack(Material.STONE)) {
+
+					@Override
+					public void onClick(ClickEvent clickEvent) {
+						new ValueRequest().requestBoolean(
+								clickEvent.getPlayer(), new BooleanListener() {
+
+									@Override
+									public void onInput(Player player,
+											boolean value) {
+										String entity = (String) Utils
+												.getInstance().getPlayerMeta(
+														player, "Entity");
+										ConfigEntity.getInstance()
+												.setDisableNormalClick(entity,
+														value);
+										plugin.reload();
+										player.sendMessage("Set disable right click to "
+												+ value);
+									}
+								});
+
+					}
+				});
+
+				inv.addButton(inv.getNextSlot(), new BInventoryButton(
+						"Add right click reward", new String[] {},
+						new ItemStack(Material.STONE)) {
+
+					@Override
+					public void onClick(ClickEvent clickEvent) {
+						Player player = clickEvent.getPlayer();
+						ArrayList<String> rewardNames = new ArrayList<String>();
+						for (Reward reward : RewardHandler.getInstance()
+								.getRewards()) {
+							rewardNames.add(reward.getRewardName());
+						}
+						new ValueRequest().requestString(player, "", Utils
+								.getInstance().convertArray(rewardNames), true,
+								new StringListener() {
+
+									@Override
+									public void onInput(Player player,
+											String value) {
+										String entity = (String) Utils
+												.getInstance().getPlayerMeta(
+														player, "Entity");
+										ArrayList<String> rewards = ConfigEntity
+												.getInstance()
+												.getRightClickedRewards(entity);
+										rewards.add(value);
+										ConfigEntity.getInstance()
+												.setRightClickedRewards(entity,
+														rewards);
+										player.sendMessage("Reward added");
+										plugin.reload();
+									}
+								});
+					}
+				});
+
+				inv.addButton(inv.getNextSlot(), new BInventoryButton(
+						"Remove right click reward", new String[] {},
+						new ItemStack(Material.STONE)) {
+
+					@Override
+					public void onClick(ClickEvent clickEvent) {
+						Player player = clickEvent.getPlayer();
+
+						new ValueRequest()
+								.requestString(
+										player,
+										"",
+										Utils.getInstance()
+												.convertArray(
+														ConfigEntity
+																.getInstance()
+																.getRightClickedRewards(
+																		(String) Utils
+																				.getInstance()
+																				.getPlayerMeta(
+																						player,
+																						"Entity"))),
+										true, new StringListener() {
+
+											@Override
+											public void onInput(Player player,
+													String value) {
+												String entity = (String) Utils
+														.getInstance()
+														.getPlayerMeta(player,
+																"Entity");
+												ArrayList<String> rewards = ConfigEntity
+														.getInstance()
+														.getRightClickedRewards(
+																entity);
+												rewards.remove(value);
+												ConfigEntity
+														.getInstance()
+														.setRightClickedRewards(
+																entity, rewards);
+												player.sendMessage("Reward removed");
+												plugin.reload();
+											}
+										});
 					}
 				});
 
